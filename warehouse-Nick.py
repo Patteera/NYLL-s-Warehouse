928af3b73842e6e95a7649e9d9eb8ad58ca653ea
Polakrit Malimai <malimai@MacBook-Pro-khxng-Polakrit.local>
2018-09-15 23:26:23 +0700

    แก้ระบบwarehouse เป็น [[1][2]] ...

--- warehouse-Nick.py
+++ warehouse-Nick.py
@@ -6,30 +6,31 @@ class Warehouse:
     def addRow(self, rows=0, grid=0):
         t1 = grid*grid
         for i in range(rows):
-            grid = []
+            grids = []
+            self.row.append(grids)
             for y in range(0, t1):
-                # self.row.append(y)
-                grid.append(y)
-            self.row.append(grid)
+                grids.append([y])
 
     def sumtable(self):
         x = 0
+        print(' \n')
         for x1 in self.row:
             x += 1
             print(x, x1)
-            print(' \n')
+            print('\n')
 
     def addProduct(self, rows=0, grid=0, product=None):
-        self.row[rows-1].pop(grid)
-        self.row[rows-1].insert(grid, product)  # (index, element)
+        # self.row[rows-1].pop(grid)
+        # self.row[rows-1].insert(grid, product)  # (index, element)
+        self.row[rows-1][grid].append(product)
 
     def deleteProduct(self, rows=0, grid=0, product=None):
-        self.row[rows-1].remove(product)
-        self.row[rows-1].insert(grid, grid)  # (index, element)
+        self.row[rows-1][grid].remove(product)
+        # self.row[rows-1].insert(grid, grid)  # (index, element)
 
 
 wh1 = Warehouse()
-wh1.addRow(5, 10)
+wh1.addRow(5, 10) #rows, grid
 wh2 = Warehouse()
 wh2.addRow(5, 10)
 wh3 = Warehouse()
@@ -42,12 +43,13 @@ wh5.addRow(20, 20)
 
 while(1):
     Eninput = raw_input('Enter Input Command  : ')
-    DtoY = ['D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y' ]
+    DtoY = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
     if (int(Eninput[0]) == 1):
 
         if (str(Eninput[1].upper()) in ['A']):
             if (int(Eninput[2:5]) in range(100, 499+1)):
-                 # rows , grid , product
+                 # rows , grid , product 
                 wh1.addProduct(int(Eninput[2]), int(Eninput[3:]), Eninput[1:])
             else:
                 print('\n Input Command Error \n')
@@ -73,11 +75,10 @@ while(1):
         else:
             print('\n Input Command Error \n')
 
-    elif (int(Eninput[0]) == 0):
-        if (str(Eninput[1]) in AtoT):
-            if (int(Eninput[2:5]) in range(100, 486+1)):
-                wh5.deleteProduct(int(Eninput[2]), int(
-                    Eninput[3:]), Eninput[1:])
+    elif (int(Eninput[0]) == 0): # cammand 0 Delete
+        if (str(Eninput[1].upper()) in ['A']):
+            if (int(Eninput[2:5]) in range(100, 499+1)): #0A100
+                wh1.deleteProduct(int(Eninput[2]), int(Eninput[3:]), Eninput[1:])
             else:
                 print('\n Input Command Error \n')
 
