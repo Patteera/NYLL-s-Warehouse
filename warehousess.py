da0ed4e91ef073f98d6e2fb4b01eec8839db84fa
Patteera <42568780+Patteera@users.noreply.github.com>
2018-09-16 03:08:34 +0700

    Add files via upload

--- warehousess.py
+++ warehousess.py
@@ -6,6 +6,9 @@ class warehouse :
         self.wh4row = []
         self.wh5row = []
 
+        self.ID = []
+        self.IDall =[]
+
         self.Product_Type = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y' ]
 
     def wh1(self,row,slot):
@@ -63,9 +66,28 @@ class warehouse :
         for k in self.wh5row:
             print k
 
+    def buildID(self):
+        for i in self.Product_Type:
+            for j in range(1,5+1):
+                for k in range(0,9+1):
+                    for l in range(0,9+1):
+                        self.ID = i+str(j)+str(k)+str(l)
+                        self.IDall.append(self.ID)
+        print self.IDall
+        print len(self.IDall)
+
+    def appendWH1(self):
+
+
+
+
 a = warehouse()
 a.wh1(5,100)
 a.wh2(5,100)
 a.wh3(5,100)
 a.wh4(7,25)
 a.wh5(20,400)
+a.buildID()
+
+
+
